a1da7cf68fa4c8fba1fd9018a81f5dfcef4abb3d
Sameen Haja Mohideen <Sameenhaja@Sameens-MacBook-Pro.local>
2018-02-23 15:08:06 +0800

    Completed exercise W6.1e

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import org.junit.Test;
 
@@ -34,6 +35,27 @@ public class UtilsTest {
         assertNotUnique(null, 1, new Integer(1));
         assertNotUnique(null, null);
         assertNotUnique(null, "a", "b", null);
+    }
+
+    @Test
+    public void isAnyNull() throws Exception {
+
+        //First Test case: No objects at all
+        containsNoNullObjects();
+
+        //Second Test case: Objects contains null
+        containsNullObjects("a", "b", null);
+
+        //Third Test case: Objects are all null
+        containsNullObjects(null, null);
+
+    }
+    private void containsNoNullObjects(Object... objects) {
+        assertFalse(Utils.isAnyNull(objects));
+    }
+
+    private void containsNullObjects(Object... objects) {
+        assertTrue(Utils.isAnyNull(objects));
     }
 
     private void assertAreUnique(Object... objects) {
